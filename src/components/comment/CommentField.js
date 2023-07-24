eb0fa4e20b6d2a5bf61d83001430ef340905dec6
MohammadAli Semnani <brutalpc78@gmail.com>
2023-07-24 19:38:47 +0330

    add username and email in form

--- src/components/comment/CommentField.js
+++ src/components/comment/CommentField.js
@@ -15,9 +15,14 @@ const CommentField = () => {
     <Container
       component="div"
       maxWidth="lg"
-      sx={{ borderRadius: 4,boxShadow: "0px 0px 25px -2px rgba(88, 88, 88, 0.65)", width: "100%"}}>
-      <Stack spacing={3} sx={{padding:"30px 40px 35px"}}>
-        <Typography  fontSize={18} fontWeight={600}>
+      sx={{
+        mt: 5,
+        borderRadius: 4,
+        boxShadow: "0px 0px 25px -2px rgba(88, 88, 88, 0.65)",
+        width: "100%",
+      }}>
+      <Stack spacing={3} sx={{padding: "30px 40px 35px"}}>
+        <Typography fontSize={18} fontWeight={600}>
           نظر خود را درباره این پست بنویسید
         </Typography>
         <TextField
@@ -38,9 +43,51 @@ const CommentField = () => {
             },
           }}
           fullWidth
+          label="نام کاربری"
+          id="fullWidth"
+        />
+        <TextField
+          sx={{
+            maxHeight: "fit-content",
+            width: "100%",
+
+            style: {textAlign: "center"},
+            "& label": {
+              left: "unset",
+              right: "1.75rem",
+              transformOrigin: "right",
+              fontSize: "1rem",
+            },
+            "& legend": {
+              textAlign: "right",
+              fontSize: ".8rem",
+            },
+          }}
+          fullWidth
+          label="ایمیل"
+          id="fullWidth"
+        />
+        <TextField
+          sx={{
+            maxHeight: "fit-content",
+            width: "100%",
+
+            style: {textAlign: "center"},
+            "& label": {
+              left: "unset",
+              right: "1.75rem",
+              transformOrigin: "right",
+              fontSize: "1rem",
+            },
+            "& legend": {
+              textAlign: "right",
+              fontSize: ".8rem",
+            },
+          }}
+          fullWidth
           multiline
           rows={4}
-          label="متن نظر"
+          label="متن مورد نظر"
           id="fullWidth"
         />
 
@@ -59,10 +106,9 @@ const CommentField = () => {
           label="Loading"
         />
 
-              <LoadingButton
-                  sx={{mb:4}}
+        <LoadingButton
+          sx={{mb: 4}}
           onClick={handleClick}
-          //   endIcon={<SendIcon sx={{textAlign:"right"}} />}
           loading={loading}
           loadingPosition="start"
           variant="contained">
